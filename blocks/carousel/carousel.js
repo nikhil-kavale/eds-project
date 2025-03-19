947452a6ca666395c134e4f422b78e7ba6fcdb09
Nikhil Kavale <nikhil.kavale@teknopoint.in>
2025-03-19 17:18:03 +0530

    added carousel css

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,7 +1,6 @@
 import Swiper1 from './swiper-bundle.min.js';
 
 export default function decorate(block) {
-//   console.log(block);
   block.classList.add('swiper');
   const swiperWrapper = document.createElement('div');
   swiperWrapper.classList.add('swiper-wrapper');
